278080fdcd9dcef6371262f20c43bb947217783d
vedarthamanuradha <vedarthamanuradha@gmail.com>
2016-06-06 06:39:11 +0000

    Add files via upload

---
+++ Exaif.java
@@ -0,0 +1,16 @@
+class Exaif
+{
+  public static void main(String args[])
+  {
+    int x=10;
+    int y=20;
+    if(x<y)
+    {
+     System.out.println("y is big");
+    }
+   else
+    {
+    System.out.println("x is big");
+    }
+  }
+} 
